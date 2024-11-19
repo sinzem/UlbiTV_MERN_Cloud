ff69d01db008f466f8e9bc166055c97a6abc87a5
sinzem <111191960+sinzem@users.noreply.github.com>
2024-11-20 00:14:47 +0200

    Add files via upload

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,6 +1,8 @@
 const Router = require("express");
 const User = require("../models/User");
+const config = require("config");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const {check, validationResult} = require("express-validator");
 
 const router = new Router();
@@ -24,7 +26,7 @@ router.post("/registration", [
             return res.status(400).json({message: `User with email ${email} already exists`});
         }
 
-        const hashPassword = await bcrypt.hash(password, 15);
+        const hashPassword = await bcrypt.hash(password, 7);
         const user = new User({email, password: hashPassword});
         await user.save();
         return res.json({message: "User was created"});
@@ -34,4 +36,35 @@ router.post("/registration", [
     }
 })
 
+router.post("/login", 
+    async (req, res) => {
+        try {
+            const {email, password} = req.body;
+            const user = await User.findOne({email});
+            if (!user) {
+                return res.status(404).json({message: "User not found"});
+            }
+            const isPassValid = bcrypt.compareSync(password, user.password);
+            if (!isPassValid) {
+                return res.status(400).json({message: "Invalid password"});
+            }
+            const token = jwt.sign({id: user.id}, config.get("secretKey"), {expiresIn: "1h"});
+            return res.json({
+                token,
+                user: {
+                    id: user.id,
+                    email: user.email,
+                    diskspace: user.diskspace,
+                    usedSpace: user.usedSpace,
+                    avatar: user.avatar
+                }   
+            })
+        } catch (e) {
+            console.log(e);
+            res.send({message: "Server error"});
+        }
+    }
+)
+
 module.exports = router; /* (подключаем в index.js) */
+
