acbf3bc9831cfe962ef90c2fccc90c63c7977688
sinzem <111191960+sinzem@users.noreply.github.com>
2024-11-19 19:14:15 +0200

    Add files via upload

---
+++ description.js
@@ -0,0 +1,18 @@
+// Пример создания облачного хранилища на Mongo, express, react, nodejs
+
+// Client
+//   - npx create-react-app .  
+
+
+// Server
+//  - npm init
+// npm i express mongoose config
+// npm i -D nodemon
+// npm i bcryptjs
+// npm i express-validator
+
+
+
+
+
+
